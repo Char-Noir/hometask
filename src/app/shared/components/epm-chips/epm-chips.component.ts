041e8c4923ed781e887afeb907d05fd25e63ce83
CharNoir <d.buryak2002@gmail.com>
2023-11-30 22:58:45 +0200

    first commit

---
+++ src/app/shared/components/epm-chips/epm-chips.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { INTEREST_CHIPS_NAMES } from '../../../app.config';
+
+@Component({
+  selector: 'epm-chips',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './epm-chips.component.html',
+  styleUrls: ['./epm-chips.component.scss']
+})
+export class EpmChipsComponent {
+  @Input() chips!: string;
+  @Input() isEditable = false;
+  @Input() isActive = false;
+
+  onClick(){
+    this.editChipsStatus.emit(this.chips);
+  }
+
+  @Output() editChipsStatus: EventEmitter<string> = new EventEmitter<string>();
+
+}
